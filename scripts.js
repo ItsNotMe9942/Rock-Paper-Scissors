f6f23cbc1a96555051904243043136a192c6d104
ItsNotMe9942 <christopher.reay85@gmail.com>
2024-07-09 12:08:59 +0100

    Update score

--- scripts.js
+++ scripts.js
@@ -10,7 +10,6 @@ const scissorsElem = document.getElementById('scissors');
 const messageElem = document.getElementById('message');
 const redTextElem = document.querySelector('.red-text');
 
-
 rockElem.addEventListener('click', () => {
   playGame('rock');
 });
@@ -25,44 +24,89 @@ scissorsElem.addEventListener('click', () => {
 
 const playGame = userMove => {
   const computerMove = pickComputerMove();
-  console.log(computerMove);
+  messageElem.remove();
+  resultElem.innerHTML = `You <img src="images/${userMove}-emoji.png"><img src="images/${computerMove}-emoji.png"> CPU`;
+  
   if (userMove === 'rock') {
     if (computerMove === 'rock') {
-      console.log('Draw');
+      console.log('draw')
+      return 'Draw';
     } else if (computerMove === 'paper') {
-      console.log('You lose!');
+      console.log('lose')
+      score.losses ++;
+      return 'You lose!';
     } else {
-      console.log('You win!');
-    };
+      console.log('win')
+      score.wins ++;
+      return 'You win!';
+    }
+
   } else if (userMove === 'paper') {
     if (computerMove === 'rock') {
-      console.log('You Win!');
+      console.log('win')
+      score.wins ++;
+      return 'You win!';
     } else if (computerMove === 'paper') {
-      console.log('Draw');
+      console.log('draw');
+      return 'Draw';
     } else {
-      console.log('You lose!');
-    };
+      console.log('lose')
+      score.losses ++;
+      return 'You lose!';
+    }
+    
   } else if (userMove === 'scissors') {
     if (computerMove === 'rock') {
-      console.log('You lose!');
+      console.log('lose')
+      score.losses ++;
+      return 'You lose!';
     } else if (computerMove === 'paper' ) {
-      console.log('You Win!');
+      console.log('win');
+      score.wins ++;
+      return 'You win!';
     } else {
-      console.log('Draw');
+      console.log('draw')
+      return 'draw';
     }
   }
+  
+  console.log(score);
+
+  
 };
 
 const pickComputerMove = () => {
   const randomNumber = Math.ceil(Math.random() * 3);
   if (randomNumber === 1) {
+    console.log('rock');
     return 'rock';
   } else if (randomNumber === 2) {
+    console.log('paper');
     return 'paper';
   } else {
+    console.log('scissors');
     return 'scissors';
   }
 };
+
+const score = {
+  wins: 0,
+  losses: 0
+};
+console.log(score)
+
+
+const updateScore = () => {
+  if (userMove = 'win') {
+
+  }
+}
+
+
+  
+
+
+
 
 
 
