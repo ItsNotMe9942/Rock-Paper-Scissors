3860ad2aafdc4f11c937cbb9c8b966116e219cfa
ItsNotMe9942 <christopher.reay85@gmail.com>
2024-07-10 15:59:38 +0100

    Amended score board

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const scoreBoardElem = document.getElementById('score-board');
-const userElem = document.getElementById('user-badge');
+const userNameElem = document.getElementById('user-badge');
 const userScoreElem = document.getElementById('user-score');
 const cpuScoreElem = document.getElementById('cpu-score');
 const resultElem = document.getElementById('result');
@@ -25,55 +25,47 @@ scissorsElem.addEventListener('click', () => {
 let score = JSON.parse(localStorage.getItem('score')) || {
   wins: 0,
   losses: 0
-}
+};
+
+updateScore();
 
 const playGame = userMove => {
   const computerMove = pickComputerMove();
   messageElem.remove();
-  resultElem.innerHTML = `You <img src="images/${userMove}-emoji.png"><img src="images/${computerMove}-emoji.png"> CPU`;
 
-  let result = ''
+  resultElem.innerHTML = `You <img src="images/${userMove}-emoji.png"><img src="images/${computerMove}-emoji.png"> CPU`;
   
   if (userMove === 'rock') {
     if (computerMove === 'rock') {
       console.log('draw');
-      
     } else if (computerMove === 'paper') {
       console.log('lose');
       score.losses ++;
-     
     } else {
       console.log('win');
       score.wins ++;
-      
     }
 
   } else if (userMove === 'paper') {
     if (computerMove === 'rock') {
       console.log('win');
       score.wins ++;
-      
     } else if (computerMove === 'paper') {
       console.log('draw');
-      
     } else {
       console.log('lose');
       score.losses ++;
-      
     }
     
   } else if (userMove === 'scissors') {
     if (computerMove === 'rock') {
       console.log('lose');
       score.losses ++;
-      
     } else if (computerMove === 'paper' ) {
       score.wins ++;
       console.log('win');
-      ;
     } else {
       console.log('draw')
-      
     }
   }
   console.log(score);
@@ -95,11 +87,42 @@ const pickComputerMove = () => {
   }
 };
 
-const updateScore = () => {
+function updateScore() {
   userScoreElem.innerHTML = score.wins;
   cpuScoreElem.innerHTML = score.losses;
   localStorage.setItem('score', JSON.stringify(score));
-}
+  
+};
+
+scoreBoardElem.addEventListener('mouseenter', () => { 
+  const resetButtonElem = document.createElement('div');
+  resetButtonElem.innerHTML = 'reset'
+  resetButtonElem.classList.add('reset-button', 'badge')
+  scoreBoardElem.appendChild(resetButtonElem);
+
+  resetButtonElem.addEventListener('click', () => {
+    score.wins = 0;
+    score.losses = 0;
+    updateScore();
+    resultElem.innerHTML = '';
+    console.log(score);
+    document.body.appendChild(messageElem)
+  })
+
+  resetButtonElem.addEventListener('mouseleave', () => {
+    scoreBoardElem.removeChild(resetButtonElem);
+  })
+
+  scoreBoardElem.addEventListener('mouseleave', () => {
+    scoreBoardElem.removeChild(resetButtonElem);
+  })
+});
+
+
+
+
+
+
 
 
 
